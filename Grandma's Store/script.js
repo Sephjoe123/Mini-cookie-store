35f0f1e59838e5870722056062667870843a5617
Bello Joseph <joexef87@gmail.com>
2023-03-09 05:18:42 +0100

    Update script.js

--- Grandma's Store/script.js
+++ Grandma's Store/script.js
@@ -1,5 +1,8 @@
 let input = document.getElementById("input");
 let outlineArea = document.getElementsByClassName("outline-img")[0]
+let imagesSection = document.getElementsByClassName("store-images")[0]
+let card = document.getElementById("img-card");
+
 
 
 outlineArea.addEventListener("mouseenter",() =>{
@@ -20,27 +23,43 @@ input.addEventListener("keyup",()=>{
      let upperCaseValue = inputValue.toUpperCase()
 
      let imageNames = Array.from(document.getElementsByClassName("card-text"));
-    //  let images = Array.from(document.getElementsByClassName("cookie-img"))
+    
+       for(let i = 0; i < imageNames.length; i++){
 
-     for(let i = 0; i < imageNames.length; i++){
-     
-     let textToBeMatched = imageNames[i].getElementsByTagName("h1")[0]
-     let cookiePrice = imageNames[i].getElementsByTagName("p")[0]
+     let matchedText = imageNames[i].getElementsByTagName("h1")[0]   
+     let text = matchedText.innerHTML;
+
+     let textArray =   text.split(", ");
+
+     for(let j = 0; j < textArray.length; j++) {
+        if(textArray[j][0].toUpperCase() === upperCaseValue.charAt(0) )
+
+        // &&  imagesSection.innerHTML.includes(textArray) == true 
+        {
+            imagesSection.innerHTML = card.innerHTML += `
+            <div id ="img-card">
+            <img class = "cookies-img" src="https://js-beginners.github.io/filter-project/img/cupcake-1.jpeg" alt="">
+            <div class="card-text">
+              <h1 class = "main-text">${textArray[j]}</h1>
+              <p>
+                $5
+              </p>
+            </div>
+            </div>
+            
+            `
+
+           
+        }   
+        else{
+            console.log(2)
+        } 
+
+        
+    }  
        
-     if(textToBeMatched.innerHTML.toUpperCase().indexOf(upperCaseValue) == - 1){
-        textToBeMatched.style.display = "";
- } 
-//      else if(cookiePrice.innerHTML.toUpperCase().indexOf(upperCaseValue) == -1){
-//         cookiePrice.style.display = ""
-//  }
-     else{
-        textToBeMatched.style.display = "none"; 
-        // cookiePrice.style.display = "none" 
-    
- }
-      }
-
-    
-
+     }
+      
+   
 })
 
