573c97eb72a100908c1de16466b7bee538176a5d
Bello Joseph <joexef87@gmail.com>
2023-03-11 01:42:04 +0100

    Create script.js

--- Grandma's Store/script.js
+++ Grandma's Store/script.js
@@ -1,9 +1,9 @@
 let input = document.getElementById("input");
 let outlineArea = document.getElementsByClassName("outline-img")[0]
 let imagesSection = document.getElementsByClassName("store-images")[0]
-let card = document.getElementById("img-card");
+let cardArea = Array.from(document.querySelectorAll(".img-card"));
 
-
+let text;
 
 outlineArea.addEventListener("mouseenter",() =>{
     if(outlineArea.classList.contains("outline-img")){
@@ -18,48 +18,14 @@ outlineArea.addEventListener("mouseleave",() =>{
     }
 })
 
+
+
 input.addEventListener("keyup",()=>{
+
      let inputValue = input.value;
      let upperCaseValue = inputValue.toUpperCase()
 
-     let imageNames = Array.from(document.getElementsByClassName("card-text"));
-    
-       for(let i = 0; i < imageNames.length; i++){
-
-     let matchedText = imageNames[i].getElementsByTagName("h1")[0]   
-     let text = matchedText.innerHTML;
-
-     let textArray =   text.split(", ");
-
-     for(let j = 0; j < textArray.length; j++) {
-        if(textArray[j][0].toUpperCase() === upperCaseValue.charAt(0) )
-
-        // &&  imagesSection.innerHTML.includes(textArray) == true 
-        {
-            imagesSection.innerHTML = card.innerHTML += `
-            <div id ="img-card">
-            <img class = "cookies-img" src="https://js-beginners.github.io/filter-project/img/cupcake-1.jpeg" alt="">
-            <div class="card-text">
-              <h1 class = "main-text">${textArray[j]}</h1>
-              <p>
-                $5
-              </p>
-            </div>
-            </div>
-            
-            `
-
-           
-        }   
-        else{
-            console.log(2)
-        } 
-
-        
-    }  
-       
-     }
-      
    
 })
+
 
